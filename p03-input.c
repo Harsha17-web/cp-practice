3d5a526d2da0c3331e7156e5d3fe1590a03463a4
harshareddy632214@gmail.com <harsha@DESKTOP-SULHRL0.localdomain>
2026-01-20 15:11:20 +0000

    add p03: integer
    gcc
    ......
    
     cp
    input-output

---
+++ p03-input.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+ int main() {
+  int a,b;
+  printf("enter two integers: ");
+  scanf("%d %d", &a,&b);
+  int product = a*b;
+  printf("product = %d\n",product);
+  return 0;
+ }
