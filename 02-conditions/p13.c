ec0867c5d7103d9733adea029bace8c03129d076
harshareddy632214@gmail.com <harsha@DESKTOP-SULHRL0.localdomain>
2026-01-23 12:44:25 +0000

    p11:salary and increment

---
+++ 02-conditions/p13.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+ int main () {
+  int d,m,y:
+  scanf("%d %d %d",&d,&m,&y);
+ 
+ if( m<1 || m>12) {
+  printf("invalid year\n"0;
+ return0;
+}
+ if( y < 1 ) {
+ printf("invalid year");
+ return 0;
+}
+ if( m== 1 &
